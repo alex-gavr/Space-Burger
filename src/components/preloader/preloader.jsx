ee271d893909cb517220fae5525a41197e310924
AG <sashadrug@mail.ru>
2022-09-10 10:25:12 +0300

    change preloader

--- src/components/preloader/preloader.jsx
+++ src/components/preloader/preloader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from './preloader.module.css';
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import logo from '../../images/logo-only-burger.svg'
 
 export const Preloader = () => {
     return( 
         <div className={styles.container}>
             <div className={styles.logContainer}>
-                <Logo />
+                <img src={logo} alt="Логотип Бургерной" className={styles.logo} />
+                <h1 className="text text_type_main-default">Загрузка сайта</h1>
             </div>
         </div>
     )
