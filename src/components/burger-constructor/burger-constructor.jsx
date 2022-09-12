d8e14f25016098aeb2e940a01b5e293a9144b282
AG <sashadrug@mail.ru>
2022-09-12 23:18:54 +0300

    count total, useReducer, add/delete ingredient

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,5 +1,5 @@
 import '@ya.praktikum/react-developer-burger-ui-components';
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useReducer} from 'react';
 import styles from './burger-constructor.module.css';
 import { ConstructorElement, Button, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { DataContext } from '../../utils/create-context';
@@ -7,8 +7,42 @@ import { OrderDetails } from '../order-details/order-details';
 import Modal from '../modal/modal';
 
 
-
 const BurgerConstructor = () => {
+    // Беру все ингредиенты из API
+    const ingredients = useContext(DataContext);
+    // Фильтрую ингредиенты чтобы была только "начинка"
+    const mainIngredients = ingredients.filter(ingredient => ingredient.type==="main");
+    // Использую начинку как initialState в useReducer
+    const initialState = mainIngredients;
+
+    // reducer для useReducer
+    const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            return [...state, action.ingredient];
+        case "DELETE":
+            return state.filter((ingredient) => ingredient._id !== action.payload)
+        default:
+            return state;
+    }
+}
+    // useReducer
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // Складываю стоимость ингрединов начинки
+    const total = state.reduce((acc, ingredient) => acc + ingredient.price, 0);
+    // Считаю общую стоимость с двумя булками
+    const totalPrice = total + ingredients[0].price*2;
+
+    // Функия добавляет новую начинку. Не используется сейчас.
+    const addAnotherIngredient = (ingredient) => {
+        dispatch({type: "ADD", ingredient});
+    }  
+    // Функция удаляет начинку
+    const deleteIngredient = (ingredient) => {
+        console.log(ingredient);
+        dispatch({type: "DELETE", payload: ingredient._id});
+    }
 
     const [isModalOpened, setIsModalOpened] = useState(false);
 
@@ -16,36 +50,39 @@ const BurgerConstructor = () => {
         setIsModalOpened(false)
     }
 
-    const data = useContext(DataContext);
-    const filteredData = data.filter(ingredient => ingredient.type==="main");
+    
     return(
         <section className={styles.wrapper}>
             <ul className={styles.container}>
+                    {/* Верхняя булка */}
                     <li className={`${styles["container-row"]} ${styles["padding-right"]}`}>
-                        <ConstructorElement type='top' isLocked={true} text={data[0].name + " (верх)"} price={data[0].price} thumbnail ={data[0].image} />
+                        <ConstructorElement type='top' isLocked={true} text={ingredients[0].name + " (верх)"} price={ingredients[0].price} thumbnail ={ingredients[0].image} />
                     </li>
+                    {/* Начинка */}
                     <div className={styles.ingredients}>
                         {
-                            filteredData.map((ingredient) => {
+                            state.map((ingredient) => {
                                 return(
-                                    <li className={styles["container-row"]} key={ingredient._id}>
+                                    <li className={styles["container-row"]} key={ingredient._id} onClick={() => deleteIngredient(ingredient)} >
                                         <DragIcon />
-                                        <ConstructorElement text={ingredient.name} price={ingredient.price} thumbnail ={ingredient.image} /> 
+                                        <ConstructorElement text={ingredient.name} price={ingredient.price} thumbnail ={ingredient.image}  /> 
                                     </li>
                                 )
                             })
                         }
                     </div>
+                    {/* Нижняя булка */}
                 <li className={`${styles["container-row"]} ${styles["padding-right"]}`}>
-                    <ConstructorElement type='bottom' isLocked={true} text={data[0].name + ' (низ)'} price={data[0].price} thumbnail ={data[0].image}  />
+                    <ConstructorElement type='bottom' isLocked={true} text={ingredients[0].name + ' (низ)'} price={ingredients[0].price} thumbnail ={ingredients[0].image}  />
                 </li>
             </ul>
-                {/* КНОПКА ЗДЕСЬ */}
             <div className={styles["container-total"]}>
                 <div className={styles["container-row"]}>
-                    <h1 className='text text_type_digits-medium'>610</h1>
+                    {/* Цена */}
+                    <h1 className='text text_type_digits-medium'>{totalPrice}</h1>
                     <CurrencyIcon type="primary" />
                 </div>
+                {/* Кнопка оформить заказ */}
                 <Button type="primary" size="large" onClick={() => setIsModalOpened(true)} > Оформить заказ </Button>
             </div>
             <Modal isOpened={isModalOpened} onClose={handleClose} >
