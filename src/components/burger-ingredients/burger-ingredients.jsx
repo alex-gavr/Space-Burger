fedfe07cfc81c2b4a8ed55bd4f382522044d10af
AG <sashadrug@mail.ru>
2022-09-13 14:21:16 +0300

    change css names in burger-ingredients + prettier

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,49 +1,82 @@
-import '@ya.praktikum/react-developer-burger-ui-components';
-import React, {useState} from 'react';
-import styles from './burger-ingredients.module.css';
-import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import ShowData from './show-data';
-import { IngredientDetails } from '../ingredient-details/ingredient-details';
-import Modal from '../modal/modal';
-
+import "@ya.praktikum/react-developer-burger-ui-components";
+import React, { useState } from "react";
+import styles from "./burger-ingredients.module.css";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import ShowData from "./show-data";
+import { IngredientDetails } from "../ingredient-details/ingredient-details";
+import Modal from "../modal/modal";
 
 const BurgerIngredients = () => {
     const [isModalOpened, setIsModalOpened] = useState(false);
     const [ingredient, setIngredient] = useState(void 0);
-    const [current, setCurrent] = React.useState('one')
+    const [current, setCurrent] = React.useState("one");
 
     const handleClose = () => {
-        setIsModalOpened(false)
-    }
+        setIsModalOpened(false);
+    };
 
-    return(
+    return (
         <section className={styles.wrapper}>
             <div className="mt-10">
-                <h1 className="text text_type_main-large mb-5">Собери бургер</h1>
-                <div className={styles["container-tabs"]}>
-                    <Tab className={styles.tab} value="one" active={current === 'one'} onClick={(setCurrent)}>
+                <h1 className="text text_type_main-large mb-5">
+                    Собери бургер
+                </h1>
+                <div className={styles.containerTabs}>
+                    <Tab
+                        className={styles.tab}
+                        value="one"
+                        active={current === "one"}
+                        onClick={setCurrent}
+                    >
                         Булки
                     </Tab>
-                    <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+                    <Tab
+                        value="two"
+                        active={current === "two"}
+                        onClick={setCurrent}
+                    >
                         Соусы
                     </Tab>
-                    <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+                    <Tab
+                        value="three"
+                        active={current === "three"}
+                        onClick={setCurrent}
+                    >
                         Начинка
                     </Tab>
                 </div>
             </div>
-            <div className={styles["sections-container"]}>
+            <div className={styles.sectionsContainer}>
                 {/* Контейнер булок */}
-                <ShowData title="Булки" type="bun" setIsModalOpened={setIsModalOpened} setIngredient={setIngredient}  />
+                <ShowData
+                    title="Булки"
+                    type="bun"
+                    setIsModalOpened={setIsModalOpened}
+                    setIngredient={setIngredient}
+                />
                 {/* Контейнер соусов */}
-                <ShowData title="Соусы" type="sauce" setIsModalOpened={setIsModalOpened} setIngredient={setIngredient} />
+                <ShowData
+                    title="Соусы"
+                    type="sauce"
+                    setIsModalOpened={setIsModalOpened}
+                    setIngredient={setIngredient}
+                />
                 {/* Контейнер начинки */}
-                <ShowData title="Начинка" type="main" setIsModalOpened={setIsModalOpened} setIngredient={setIngredient} />
+                <ShowData
+                    title="Начинка"
+                    type="main"
+                    setIsModalOpened={setIsModalOpened}
+                    setIngredient={setIngredient}
+                />
             </div>
-            <Modal title='Детали ингредиента' isOpened={isModalOpened} onClose={handleClose} >
+            <Modal
+                title="Детали ингредиента"
+                isOpened={isModalOpened}
+                onClose={handleClose}
+            >
                 <IngredientDetails ingredient={ingredient} />
             </Modal>
         </section>
-    )
-}
+    );
+};
 export default BurgerIngredients;
--- src/components/burger-ingredients/show-data.jsx
+++ src/components/burger-ingredients/show-data.jsx
@@ -1,49 +1,65 @@
-import '@ya.praktikum/react-developer-burger-ui-components';
-import React, {useContext} from 'react';
-import PropTypes from 'prop-types';
-import styles from './burger-ingredients.module.css';
-import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
-import { DataContext } from '../../utils/create-context';
-
-
-
-
+import "@ya.praktikum/react-developer-burger-ui-components";
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import styles from "./burger-ingredients.module.css";
+import {
+    CurrencyIcon,
+    Counter,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { DataContext } from "../../utils/create-context";
 
 const ShowData = (props) => {
-
     const data = useContext(DataContext);
-    const filteredData = data.filter(ingredient => ingredient.type === props.type);
+    const filteredData = data.filter(
+        (ingredient) => ingredient.type === props.type
+    );
 
-
-    return(
+    return (
         <div>
-            <h3 className='text text_type_main-medium'>{props.title}</h3>
+            <h3 className="text text_type_main-medium">{props.title}</h3>
             <ul className={styles.container}>
-            {
-                filteredData.map((ingredient) => {
-                    return(
-                        <li key={ingredient._id} className={styles["container-element"]} onClick={()=> {props.setIngredient(ingredient); props.setIsModalOpened(true)}}  >
-                            <div className={styles["container-image"]}>
-                                <img src={ingredient.image} alt={ingredient.name} />
+                {filteredData.map((ingredient) => {
+                    return (
+                        <li
+                            key={ingredient._id}
+                            className={styles.containerElement}
+                            onClick={() => {
+                                props.setIngredient(ingredient);
+                                props.setIsModalOpened(true);
+                            }}
+                        >
+                            <div className={styles.containerImage}>
+                                <img
+                                    src={ingredient.image}
+                                    alt={ingredient.name}
+                                />
                             </div>
-                            <div className={styles["container-price"]}>
-                                <p className='text text_type_digits-default'>{ingredient.price}</p>
+                            <div className={styles.containerPrice}>
+                                <p className="text text_type_digits-default">
+                                    {ingredient.price}
+                                </p>
                                 <CurrencyIcon type="primary" />
                             </div>
-                            <p className='text text_type_main-default'>{ingredient.name}</p>
-                            {ingredient.__v >= 1 ? <Counter count={ingredient.__v} size="default" /> : null }
+                            <p className="text text_type_main-default">
+                                {ingredient.name}
+                            </p>
+                            {ingredient.__v >= 1 ? (
+                                <Counter
+                                    count={ingredient.__v}
+                                    size="default"
+                                />
+                            ) : null}
                         </li>
-                    )
-                })
-            }
+                    );
+                })}
             </ul>
         </div>
-    )
-}
+    );
+};
 ShowData.propTypes = {
     type: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     setIngredient: PropTypes.func.isRequired,
-    setIsModalOpened: PropTypes.func.isRequired
-}
-export default ShowData
+    setIsModalOpened: PropTypes.func.isRequired,
+};
+export default ShowData;
